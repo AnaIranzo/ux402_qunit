c7a38afdc8f01ebc33b928b437155c4a42243766
Ana E. Iranzo Molina <aeiranzom@gmail.com>
2024-02-05 18:58:00 +0000

    first tests

---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	"student6820/com/sap/training/ux402/qunit/ux402_qunit/test/unit/controller/Main.controller",
+    "student6820/com/sap/training/ux402/qunit/ux402_qunit/test/unit/model/Formatter"
+
+], function () {
+	"use strict";
+});
---
+++ webapp/test/unit/unitTests.qunit.js
@@ -0,0 +1,13 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"student6820/com/sap/training/ux402/qunit/ux402_qunit/test/unit/AllTests"
+
+	], function () {
+		QUnit.start();
+	});
+});
